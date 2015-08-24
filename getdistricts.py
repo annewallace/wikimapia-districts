f60ca73756530ef5fff5c74f67ea90d7e2b464e0
mk-99 <mklochkov@gmail.com>
2015-08-24 20:27:48 +0300

    Main functionality added

--- getdistricts.py
+++ getdistricts.py
@@ -4,10 +4,32 @@
 # Get districts from wikimapia, generate lists of coordinates
 
 import sys
+import pymapia
 from codecs import getreader, getwriter
+
 
 sys.stdin = getreader('utf-8')(sys.stdin)
 sys.stdout = getwriter('utf-8')(sys.stdout)
 
+def dumpclean(obj):
+    if type(obj) == dict:
+        for k, v in obj.items():
+            if hasattr(v, '__iter__'):
+                print k
+                dumpclean(v)
+            else:
+                print '%s : %s' % (k, v)
+    elif type(obj) == list:
+        for v in obj:
+            if hasattr(v, '__iter__'):
+                dumpclean(v)
+            else:
+                print v
+    else:
+        print obj
 
-print "It will be an output here"
+session = pymapia.PyMapia("69911E52-1583C8FE-1C998725-04FC0632-4DB32EEE-50D8E7E4-713CE9D8-46BB4FDE")
+o = session.get_place_by_id(9762956)
+
+print o['title']
+print dumpclean(o)
